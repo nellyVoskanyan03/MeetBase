16e68b57c8d8ae3401f16016c6852332c41bf774
nelly <nelly@miro.com>
2026-04-23 14:28:32 +0400

    meet_ms project create

---
+++ meet_ms/src/main/java/git/meet_base/meet_ms/MeetMsApplication.java
@@ -0,0 +1,13 @@
+package git.meet_base.meet_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetMsApplication.class, args);
+    }
+
+}
---
+++ meet_ms/src/test/java/git/meet_base/meet_ms/MeetMsApplicationTests.java
@@ -0,0 +1,13 @@
+package git.meet_base.meet_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
