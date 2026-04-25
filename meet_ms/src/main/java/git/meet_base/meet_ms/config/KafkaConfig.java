2439b0edc74b258eb5a727fa5fd50a4a4f48b0c6
nelly <nelly@miro.com>
2026-04-25 19:55:11 +0400

    meet_ms: adding meet_ms and postgres in compose

--- meet_ms/src/main/java/git/meet_base/meet_ms/config/KafkaConfig.java
+++ meet_ms/src/main/java/git/meet_base/meet_ms/config/KafkaConfig.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
-    @Value(value = "${spring.kafka.producer.bootstrap-servers}")
+    @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
 
     @Bean
