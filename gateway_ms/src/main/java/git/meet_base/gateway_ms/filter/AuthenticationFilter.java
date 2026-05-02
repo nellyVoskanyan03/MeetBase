dd375be0141d73191c2e2d913ff6e822b7c2d105
nelly <nelly@miro.com>
2026-05-02 16:55:35 +0400

    gateway_ms: adding AuthenticationFilter with JWT token

---
+++ gateway_ms/src/main/java/git/meet_base/gateway_ms/filter/AuthenticationFilter.java
@@ -0,0 +1,68 @@
+package git.meet_base.gateway_ms.filter;
+
+import git.meet_base.gateway_ms.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    private final JwtUtil jwtUtil;
+
+    public AuthenticationFilter(JwtUtil jwtUtil) {
+        super(Config.class);
+        this.jwtUtil = jwtUtil;
+    }
+
+    public static class Config {
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+
+            ServerHttpRequest request = exchange.getRequest();
+            String path = request.getURI().getPath();
+            String method = request.getMethod().name();
+
+            if (path.contains("/api/v1/users/login") || path.contains("/v3/api-docs")) {
+                return chain.filter(exchange);
+            }
+
+            if (path.equals("/api/v1/users") && "POST".equals(method)) {
+                return chain.filter(exchange);
+            }
+
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                authHeader = authHeader.substring(7);
+            }
+
+            try {
+                jwtUtil.validateToken(authHeader);
+
+                Claims claims = jwtUtil.extractAllClaims(authHeader);
+                exchange.getRequest().mutate()
+                        .header("X-User-Id", claims.getSubject())
+                        .header("X-User-Role", claims.get("role", String.class))
+                        .build();
+
+            } catch (Exception e) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+}
---
+++ gateway_ms/src/main/java/git/meet_base/gateway_ms/util/JwtUtil.java
@@ -0,0 +1,30 @@
+package git.meet_base.gateway_ms.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
